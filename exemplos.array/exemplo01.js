cc575562f248f1f4c3ba6642f4b1a7683b1c9cfe
Kevin Goulart Trevisan <kgtbra@gmail.com>
2026-05-21 14:59:39 -0300

    mais exemplos array

--- exemplos.array/exemplo01.js
+++ exemplos.array/exemplo01.js
@@ -1,3 +1,44 @@
 //Kevin goulart
 
-//exemplos-array
+//criando arrays com nome de frutas e sucos
+let sucos = [ "Maça", "Banana", "Laranja", "Uva"];
+console.log(sucos[0]);
+
+//criado um array de números
+let numeros = [1, 2, 3, 4, 5];
+console.log(numeros[2]);
+
+//acessando um elemento especifico do array
+let animais = ["Cachorro", "Gato", "Pássaro", "Peixe"];
+console.log(animais[4]);
+
+//alterando um elemento do array 
+let times = ["Flamingo", "Palmeiras", "São paulo", "Corintias"];
+times[2] = "Vasco";
+console.log(times);
+
+//descobrindo o tamanho do array
+let games = ["FIFA", "CALL of DUTY", "Minecraft", "Fortnite"];
+console.log(games.lench);
+
+//adicionando um elemeno ao final do array
+let cores = ["Vermelho", "Azul", "Verde"];
+cores.push("Amarelo");
+console.log(cores);
+
+//removendo o ultimo elemento do array
+let carros = ["fusca", "gol", "civic", "corolla"];
+carros.pop();
+console.log(carros);
+
+//removendo o primeiro elemento do array
+let cidades = ["são paulo", "rio de janeiro", "belo horizonte", "curitiba"];
+cores.shift();
+console.log(cidades);
+
+//adicioando um elemento no inicio do array
+let paises = ["brasil", "argentina", "chile"];
+paises.unshift("uruguai");
+console.log(paises);
+
+//
